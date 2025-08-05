d058058ace4ad44105c1e837496685843166425c
Lars-Klinkenberg <klinkenberg.lars@gmail.com>
2025-08-05 19:05:02 +0200

    added event listeners

--- multiselect.js
+++ multiselect.js
@@ -7,11 +7,11 @@ class Option {
 }
 
 class Multiselect {
-  selectedOptions = [];
+  selectedItems = [];
   template = {
     parentContainer: null,
     selectContainer: null,
-    placeholderContainer: null,
+    headerPlaceholderContainer: null,
     inputContainer: null,
     selectOptionsContainer: null,
     maxSelectedContainer: null,
@@ -57,62 +57,68 @@ class Multiselect {
 
     console.log("this ", this);
     this.initTemplate();
+    this.addEventListeners();
   }
 
   initTemplate() {
     console.log("Multiselect initialized in", this.parentContainer);
     this.template.parentContainer = this.loadTemplate();
+    this.template.parentContainer.id = this.name;
 
-    this.template.maxSelectedContainer = this.template.parentContainer.querySelector(
-      ".multi-select-header-max"
-    );
+    this.template.maxSelectedContainer =
+      this.template.parentContainer.querySelector(".multi-select-header-max");
     if (this.template.maxSelectedContainer) {
       this.template.maxSelectedContainer.textContent = this.maxSelectedText;
     }
 
-    this.template.placeholderContainer = this.template.parentContainer.querySelector(
-      ".multi-select-header-placeholder"
-    );
-    if (this.template.placeholderContainer) {
-      this.template.placeholderContainer.textContent = this.config.placeholder;
+    this.template.headerPlaceholderContainer =
+      this.template.parentContainer.querySelector(
+        ".multi-select-header-placeholder"
+      );
+    if (this.template.headerPlaceholderContainer) {
+      this.template.headerPlaceholderContainer.textContent =
+        this.config.placeholder;
     }
 
-    this.template.selectOptionsContainer = this.template.parentContainer.querySelector(
-      ".multi-select-options"
-    );
+    this.template.selectOptionsContainer =
+      this.template.parentContainer.querySelector(".multi-select-options");
     if (this.template.selectOptionsContainer) {
-        if(this.config.selectAll) {
-            this.template.selectOptionsContainer.innerHTML = this.selectAllHtml;
-        }
-        this.template.selectOptionsContainer.innerHTML += this.optionsHtml;
+      if (this.config.selectAll) {
+        this.template.selectOptionsContainer.innerHTML = this.selectAllHtml;
+      }
+      this.template.selectOptionsContainer.innerHTML += this.optionsHtml;
     }
 
-    console.log("Template loaded", this.template);
+    console.log("Template loaded", this.template.parentContainer);
     this.selectElement.replaceWith(this.template.parentContainer);
   }
 
   loadTemplate() {
     return document
       .getElementById("multiselect-template")
-      .content.cloneNode(true);
+      .content.firstElementChild.cloneNode(true);
   }
 
   get optionsHtml() {
     let html = "";
     this.config.data.forEach((item) => {
-      html += `
-        <div class="multi-select-option}" data-value="${item.value}">
+      let option = document.createElement("div");
+      option.className = "multi-select-option";
+      option.setAttribute("data-value", item.value);
+      option.innerHTML = `
             <span class="multi-select-option-radio"></span>
-                <span class="multi-select-option-text">
-                    ${item.html ? item.html : item.text}
-                </span>
-        </div>`;
+            <span class="multi-select-option-text">
+                ${item.html ? item.html : item.text}
+            </span>
+        `;
+      html += option.outerHTML;
     });
+
     return html;
   }
 
-  get selectAllHtml(){
-    return `<div class="multi-select-all">
+  get selectAllHtml() {
+    return `<div class="multi-select-all" data-value="select-all">
                 <span class="multi-select-option-radio"></span>
                 <span class="multi-select-option-text">Select all</span>
             </div>`;
@@ -120,6 +126,116 @@ class Multiselect {
 
   get maxSelectedText() {
     if (!this.config.max) return "";
-    return (this.selectedValues?.length || "0") + "/" + this.config.max;
+    return (this.selectedItems?.length || "0") + "/" + this.config.max;
+  }
+
+  addEventListeners() {
+    console.log("addEventlisteneres", this.template);
+    this.template.selectOptionsContainer.childNodes.forEach((option) => {
+      option.addEventListener("click", (event) => {
+        this.optionClick(event, option);
+      });
+    });
+    console.log("Event listeners added ", this.name);
+    document.getElementById(this.name)?.addEventListener("click", (event) => {
+      console.log("Parent container clicked", event);
+      if (!event.target.classList.contains("multi-select-header")) return;
+      let header = event.target;
+
+      if (header.classList.contains("multi-select-header-active")) {
+        header.classList.remove("multi-select-header-active");
+      } else {
+        header.classList.add("multi-select-header-active");
+      }
+    });
+  }
+
+  optionClick(event, option) {
+    console.log("Option clicked", option.dataset.value, this.selectedItems);
+
+    // select all functionality
+    if (option.dataset.value === "select-all") {
+      if (this.selectedItems.length === this.config.data.length) {
+        this.selectedItems = [];
+        this.template.selectOptionsContainer.childNodes.forEach((opt) => {
+          opt.classList.remove("multi-select-selected");
+          this.toggleDisabledOptions();
+        });
+      } else {
+        this.selectedItems = this.config.data.map((item) => item.value);
+        this.template.selectOptionsContainer.childNodes.forEach((opt) => {
+          opt.classList.add("multi-select-selected");
+          this.toggleDisabledOptions();
+        });
+      }
+      return;
+    }
+
+    if (this.selectedItems.includes(option.dataset.value)) {
+      console.warn("Unselecting item", option.dataset.value, this.selectedItems);
+      this.selectedItems = this.selectedItems.filter((item) => item !== option.dataset.value);
+      console.warn("Unselecting item", option.dataset.value, this.selectedItems);
+      option.classList.remove("multi-select-selected");
+      this.template.maxSelectedContainer.textContent = this.maxSelectedText;
+      this.toggleDisabledOptions();
+      this.updateSelectedItems();
+      return;
+    }
+
+    if (this.config.max && this.selectedItems.length >= this.config.max) {
+      console.warn("Max selection limit reached");
+      this.toggleDisabledOptions();
+      return;
+    }
+
+    this.selectedItems.push(option.dataset.value);
+    this.template.maxSelectedContainer.textContent = this.maxSelectedText;
+    if (!option.classList.contains("multi-select-selected")) {
+      option.classList.add("multi-select-selected");
+    }
+    this.updateSelectedItems();
+
+    this.toggleDisabledOptions();
+  }
+
+  updateSelectedItems() {
+    console.error("Updating selected items", this.selectedItems);
+    let html = this.selectedItems
+      .map(
+        (value) => `<span  class="multi-select-header-option">${value}</span>`
+      )
+      .join("");
+    console.log("Selected items HTML", html);
+
+    console.log("Max selected text", this.template.headerPlaceholderContainer);
+
+    this.template.maxSelectedContainer.textContent = this.maxSelectedText;
+    this.template.headerPlaceholderContainer.innerHTML = html;
+  }
+
+  disableNonSelectedOptions() {
+    console.log("Disabling non-selected options");
+    this.template.selectOptionsContainer.childNodes.forEach((option) => {
+      if (!this.selectedItems.includes(option.dataset.value)) {
+        option.classList.add("multi-select-disabled");
+      } else {
+        option.classList.remove("multi-select-disabled");
+      }
+    });
+  }
+
+  enableAllOptions() {
+    console.log("Enabling all options");
+    this.template.selectOptionsContainer.childNodes.forEach((option) => {
+      option.classList.remove("multi-select-disabled");
+    });
+  }
+
+  toggleDisabledOptions() {
+    if (this.config.max && this.config.max <= this.selectedItems.length) {
+      this.disableNonSelectedOptions();
+    } else {
+      this.enableAllOptions();
+    }
   }
 }
